a4455e0b733feaba649ee39294d3a0d037985014
sth6197 <sth6197@gmail.com>
2024-06-10 19:44:06 +0900

    (2024.06.10) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class SingleLinkedList
+{
+private:
+    int size;
+
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    Node* head;
+
+public:
+    void SingleLingkedList()
+    {
+        size = 0;
+        head = nullptr;
+    }
+
+    void PushFront(T data)
+    {
+        if (head == nullptr)
+        {
+            head = new Node;
+            head->data = data;
+            head->next = nullptr;
+        }
+        else
+        {
+            Node* newnode = new Node;
+            newnode->next = head;
+            newnode->data = data;
+            head = newnode;
+        }
+        size++;
+    }
+
+    void Show()
+    {
+        Node* crntnode = head;
+
+        while(crntnode != nullptr)
+        {
+            cout << crntnode->data << endl;
+            crntnode = crntnode->next;
+        }
+    }
+
+};
+
+int main()
+{
+    SingleLinkedList<int> single;
+
+
+
+
+}
